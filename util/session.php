f10b1b9c0d06194ff008c38298faf463bff89eb6
hcz <h.cz@qq.com>
2015-04-01 17:15:09 +0800

    add session_create timeout

--- util/session.php
+++ util/session.php
@@ -1,18 +1,23 @@
 <?php
     session_start();
 
-    if (isset($_SESSION['session_time'])) {
-        if (time() - $_SESSION['session_time'] > 10) {
-            // timeout
-            // reset session
-            session_destroy();
-            session_start();
-        }
+    if (
+        (!isset($_SESSION['session_time']))
+        || (!isset($_SESSION['session_create']))
+        || (time() - $_SESSION['session_time'] > 60 * 60)
+        || (time() - $_SESSION['session_create'] > 60 * 60 * 24)
+    ) {
+        // reset session
+        session_destroy();
+        session_start();
+
+        $_SESSION['session_create'] = time();
     }
 
     $_SESSION['session_time'] = time();
 
     // $_SESSION['test'] = $_SESSION['test'] . '.';
-    // echo $_SESSION['session_time'];
+    // echo $_SESSION['session_time'] . ' ';
+    // echo $_SESSION['session_create'] . ' ';
     // echo $_SESSION['test'];
 ?>
