068d0a8a48517b3286a7f528fc57e329ac56c610
hcz <h.cz@qq.com>
2015-04-05 00:20:00 +0800

    add book.php and cat.php

---
+++ ajax/book.php
@@ -0,0 +1,32 @@
+<?php
+    require_once '../util/ajax.php';
+    require_once '../util/session.php';
+    require_once '../util/db_book.php';
+
+    $book_id = ajax_arg('book_id', FILTER_VALIDATE_REGEXP, $filter_number);
+
+    $book_info = db_book_get($book_id);
+
+    if ($book_info) {
+        echo ajax_gen(
+            'get_success', true,
+
+            'book_id', $book_info['book_id'],
+
+            'name', $book_info['name'],
+            'detail', $book_info['detail'],
+            'price', $book_info['price'],
+            'sold', $book_info['sold'],
+            'inventory', $book_info['inventory'],
+
+            'date_add', $book_info['date_add']
+
+            // TODO: order history & comments?
+            // TODO: catalog path?
+        );
+    } else {
+        echo ajax_gen(
+            'get_success', false
+        );
+    }
+?>
---
+++ ajax/cat.php
@@ -0,0 +1,26 @@
+<?php
+    require_once '../util/ajax.php';
+    require_once '../util/session.php';
+    require_once '../util/db_cat.php';
+
+    $cat_id = ajax_arg('cat_id', FILTER_VALIDATE_REGEXP, $filter_number);
+
+    $cat_info = db_cat_get($cat_id);
+
+    if ($cat_info) {
+        echo ajax_gen(
+            'get_success', true,
+
+            'cat_id', $cat_info['cat_id'],
+
+            'name', $cat_info['name'],
+            'detail', $cat_info['detail']
+
+            // TODO: catalog path?
+        );
+    } else {
+        echo ajax_gen(
+            'get_success', false
+        );
+    }
+?>
